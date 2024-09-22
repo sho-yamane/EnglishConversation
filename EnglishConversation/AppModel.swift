0bd84c95ebe2d71393ddd5cca002dc5df9ba50af
sho-yamane <sns.yamane@gmail.com>
2024-09-22 11:25:11 +0900

    Initial Commit

---
+++ EnglishConversation/AppModel.swift
@@ -0,0 +1,21 @@
+//
+//  AppModel.swift
+//  EnglishConversation
+//
+//  Created by Sho Yamane on 2024/09/22.
+//
+
+import SwiftUI
+
+/// Maintains app-wide state
+@MainActor
+@Observable
+class AppModel {
+    let immersiveSpaceID = "ImmersiveSpace"
+    enum ImmersiveSpaceState {
+        case closed
+        case inTransition
+        case open
+    }
+    var immersiveSpaceState = ImmersiveSpaceState.closed
+}
